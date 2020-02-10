c336945ae6a111ab3733c8ea160e32b91488ef98
shoto-hatayama <hatayama.s00045@gmail.com>
2020-02-11 02:29:31 +0900

    initial commit

---
+++ resources/js/components/defaltcomponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+      <ul>
+        <li>リンゴ</li>
+        <li>りんご</li>
+        <li>{{fluits}}</li>
+      </ul>
+    </div>
+</template>
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <example-components></example-components>
+    <defalt-component></defalt-component>
+@endsection
